b80aa775a415fa56af27e1a123802c2a18e98cb3
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-08-27 16:58:32 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
-import {GLTFLoader} from "./three.js-r132/examples/jsm/loaders/GLTFLoader.js";
+import {GLTFLoader} from "./libs/faceapi/three.js-r132/examples/jsm/loaders/GLTFLoader.js";
 import * as THREE from "./three.js-r132/build/three.module.js";
 
 //const THREE = window.MINDAR.IMAGE? window.MINDAR.IMAGE.THREE: window.MINDAR.FACE.THREE;
