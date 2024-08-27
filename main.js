ce86dd85cc3f5267eecda66505e45be99d536431
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-08-27 16:18:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import {loadGLTF} from "libs/faceapi/loader.js";
-import * as THREE from 'libs/faceapi/three.js-r132/build/three.module.js';
-import {ARButton} from 'libs/faceapi/three.js-r132/examples/jsm/webxr/ARButton.js';
+import {loadGLTF} from "./libs/faceapi/loader.js";
+import * as THREE from './libs/faceapi/three.js-r132/build/three.module.js';
+import {ARButton} from './libs/faceapi/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 const normalizeModel = (obj, height) => {
   // scale it according to height
