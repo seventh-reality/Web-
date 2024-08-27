0d955c5c313673d686eda49056df9ea8ecd4890b
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-08-27 16:13:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {loadGLTF} from "libs/faceapi/loader.js";
-import * as THREE from '../../libs/three.js-r132/build/three.module.js';
+import * as THREE from 'libs/faceapi/three.js-r132/build/three.module.js';
 import {ARButton} from '../../libs/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 const normalizeModel = (obj, height) => {
